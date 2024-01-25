d4f04a9ba3e2b0b9abd024cb4914562d24062f46
Dunaeff Eugenii <56464554+Gargooie@users.noreply.github.com>
2024-01-26 01:30:16 +0400

    add gitignore

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-x = int(input("Введите кол-во мальчиков: "))
-y = int(input("Введите кол-во девочек: "))
-
-a = ""
-
-for i in range((x + y) // 2):
-  a += "B"
-  x -= 1
-  a += "G"
-  y -= 1
-print(a)
-print()
+print("hi")
